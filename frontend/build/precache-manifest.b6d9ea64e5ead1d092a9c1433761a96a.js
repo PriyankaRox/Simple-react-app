4c45c4a6d659ef498cf92b51e23706269a3c862e
Priyanka Rokhade <priyankavrokhade@gmail.com>
2023-08-02 13:37:47 +0530

    simple-app-code

---
+++ frontend/build/precache-manifest.b6d9ea64e5ead1d092a9c1433761a96a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b602d553544a60bb96b08c90c796350e",
+    "url": "/Simple-react-app/index.html"
+  },
+  {
+    "revision": "ab2e4807da46ffa14d8b",
+    "url": "/Simple-react-app/static/css/main.d0c2da22.chunk.css"
+  },
+  {
+    "revision": "36afc103decb12906289",
+    "url": "/Simple-react-app/static/js/2.fef97480.chunk.js"
+  },
+  {
+    "revision": "ab2e4807da46ffa14d8b",
+    "url": "/Simple-react-app/static/js/main.a44ea8df.chunk.js"
+  },
+  {
+    "revision": "9a9c20e355b9cb25afb5",
+    "url": "/Simple-react-app/static/js/runtime-main.761b8079.js"
+  }
+]);
